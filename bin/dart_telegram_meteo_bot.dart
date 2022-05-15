29ca4e587a19bbc7887ee940ada0550ff11956e3
Bachar-official <cjba4ar@gmail.com>
2022-05-15 17:01:52 +0500

    init Dart commit

---
+++ bin/dart_telegram_meteo_bot.dart
@@ -0,0 +1,5 @@
+import 'package:dart_telegram_meteo_bot/dart_telegram_meteo_bot.dart' as dart_telegram_meteo_bot;
+
+void main(List<String> arguments) {
+  print('Hello world: ${dart_telegram_meteo_bot.calculate()}!');
+}
